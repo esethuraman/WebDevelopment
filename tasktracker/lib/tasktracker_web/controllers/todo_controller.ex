ce58382151d3c199ebb5d432d984e73d59190f93
Elavazhagan Sethuraman <elattp@gmail.com>
2018-02-21 02:34:29 +0000

    attributions added. fix in todo controller for edit html did

--- tasktracker/lib/tasktracker_web/controllers/todo_controller.ex
+++ tasktracker/lib/tasktracker_web/controllers/todo_controller.ex
@@ -36,20 +36,21 @@ defmodule TasktrackerWeb.TodoController do
   def edit(conn, %{"id" => id}) do
     todo = TrackerApp.get_todo!(id)
     changeset = TrackerApp.change_todo(todo)
-    all_users = Accounts.list_users
-    render(conn, "new.html", todo: todo, changeset: changeset, all_users: all_users)
+    all_users = Accounts.list_users()
+    render(conn, "edit.html", todo: todo, changeset: changeset, all_users: all_users)
   end
 
   def update(conn, %{"id" => id, "todo" => todo_params}) do
     todo = TrackerApp.get_todo!(id)
-    all_users = Accounts.list_users
+    all_users = Accounts.list_users()
     case TrackerApp.update_todo(todo, todo_params) do
+      
       {:ok, todo} ->
         conn
         |> put_flash(:info, "Todo updated successfully.")
         |> redirect(to: todo_path(conn, :show, todo))
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", todo: todo, changeset: changeset, all_users: all_users)
+        render(conn, "edit.html", todo: todo, changeset: changeset, all_users: all_users)
     end
   end
 
@@ -58,8 +59,8 @@ defmodule TasktrackerWeb.TodoController do
     {:ok, _todo} = TrackerApp.delete_todo(todo)
 
     conn
-    |> put_flash(:info, "Todo deleted successfully.")
-    |> redirect(to: todo_path(conn, :index))
+    |> put_flash(:info, "Task deleted successfully.")
+    |> redirect(to: page_path(conn, :dashboard))
   end
 
 end
